4217fa9c9e82bba2179c3717b6b25a73728f3bad
Afnan Khalid <afnank19@gmail.com>
2025-02-13 23:42:37 +0500

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+## Create AFN app
+
+#### Quick Start
+
+`npx @afnank19/create-afn-app <project-name>`
+
+Two folders will be created namely frontend and backend.
+Change your directory to each, and run
+
+`npm install`
+
+and hack away!
---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const { program } = require("commander");
+const fs = require("fs-extra"); // Optional: for file operations
+const path = require("path");
+
+program
+  .version("0.0.1")
+  .argument("<project-name>", "Name of the project")
+  .action((projectName) => {
+    console.log("Project Name is: ", projectName);
+
+    const projectPath = path.resolve(process.cwd(), projectName);
+    const templatePath = path.join(__dirname, "template");
+
+    // Copy the entire template folder to the new project directory
+    fs.copySync(templatePath, projectPath);
+
+    console.log("Project setup complete!");
+  });
+
+program.parse(process.argv);
